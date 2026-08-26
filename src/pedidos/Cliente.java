0f5e1e1cb6849ca35ae71f4e1eeefa987c9f5f98
André Masoni <andremasonifraile@gmail.com>
2026-08-26 20:42:40 -0300

    add Cliente class with constructor and getters

--- src/pedidos/Cliente.java
+++ src/pedidos/Cliente.java
@@ -1,5 +1,37 @@
 package pedidos;
 
-
 public class Cliente {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private String endereco;
+
+    public Cliente(int id, String nome, String telefone, String cpf, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
 }
