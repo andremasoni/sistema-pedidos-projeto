be9ae9754dd57d0770232c4e5b3e72b1015af740
André Masoni <andremasonifraile@gmail.com>
2026-08-26 23:18:31 -0300

    refactor: update Pedido

--- src/pedidos/Pedido.java
+++ src/pedidos/Pedido.java
@@ -1,4 +1,32 @@
 package pedidos;
 
 public class Pedido {
+    private int id;
+    private Cliente cliente;
+    private String nomePedido;
+    private String dataPedido;
+
+    public Pedido (int id,Cliente cliente, String nomePedido,String dataPedido){
+        this.id = id;
+        this.cliente = cliente;
+        this.nomePedido = nomePedido;
+        this.dataPedido = dataPedido;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public int getId(){
+        return  id;
+    }
+
+    public String getNomePedido() {
+        return nomePedido;
+    }
+
+    public String getDataPedido() {
+        return dataPedido;
+    }
+
 }
