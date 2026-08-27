c9545d9f8bba3e990647e3234254ef745e5dd28d
André Masoni <andremasonifraile@gmail.com>
2026-08-26 23:27:18 -0300

    add: Structure, Class -> Produto

--- src/pedidos/Produto.java
+++ src/pedidos/Produto.java
@@ -1,4 +1,31 @@
 package pedidos;
 
 public class Produto {
+    private int id;
+    private String nomeProduto;
+    private double valorProduto;
+    private int produtoEstoque;
+
+    public Produto(int id, String nomeProduto, double valorProduto, int produtoEstoque) {
+        this.id = id;
+        this.nomeProduto = nomeProduto;
+        this.valorProduto = valorProduto;
+        this.produtoEstoque = produtoEstoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public double getValorProduto() {
+        return valorProduto;
+    }
+
+    public int getProdutoEstoque() {
+        return produtoEstoque;
+    }
 }
